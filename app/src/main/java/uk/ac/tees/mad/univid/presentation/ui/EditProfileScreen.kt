af875ee85620dcab19bbb735c026e106a260506e
chiru-kiran <162730023+chiru-kiran@users.noreply.github.com>
2024-12-18 01:54:27 +0530

    Commit 22

--- app/src/main/java/uk/ac/tees/mad/univid/presentation/ui/EditProfileScreen.kt
+++ app/src/main/java/uk/ac/tees/mad/univid/presentation/ui/EditProfileScreen.kt
@@ -1,18 +1,23 @@
 package uk.ac.tees.mad.univid.presentation.ui
 
+import android.net.Uri
+import android.os.Environment
+import android.widget.Toast
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
-import androidx.compose.material.icons.rounded.KeyboardArrowRight
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -27,13 +32,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.core.content.FileProvider
 import androidx.navigation.NavHostController
 import coil.compose.AsyncImage
 import uk.ac.tees.mad.univid.R
 import uk.ac.tees.mad.univid.presentation.AppViewModel
-import uk.ac.tees.mad.univid.presentation.component.ApplicationNavigationItems
+import java.io.File
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -42,9 +49,39 @@ fun EditProfileScreen(viewModel: AppViewModel, navController: NavHostController)
     var name = user.value!!.name
     var email = user.value!!.email
     var number = user.value!!.number
+    val context = LocalContext.current
     var newName by remember { mutableStateOf(name) }
     var newEmail by remember { mutableStateOf(email) }
     var newNumber by remember { mutableStateOf(number) }
+
+    var imageUri by remember { mutableStateOf<Uri?>(null) }
+
+    val photoFile = remember {
+        File.createTempFile("IMG_", ".jpg", context.getExternalFilesDir(Environment.DIRECTORY_PICTURES))
+    }
+
+    val photoUri = FileProvider.getUriForFile(
+        context,
+        "${context.packageName}.fileprovider",
+        photoFile
+    )
+
+
+    val takePictureLauncher = rememberLauncherForActivityResult(ActivityResultContracts.TakePicture()) { success ->
+        if (success) {
+            imageUri = photoUri
+        }
+    }
+    val cameraPermissionLauncher = rememberLauncherForActivityResult(
+        contract = ActivityResultContracts.RequestPermission(),
+        onResult = { isGranted ->
+            if (isGranted) {
+                takePictureLauncher.launch(photoUri)
+            } else {
+                Toast.makeText(context, "Camera permission is required", Toast.LENGTH_SHORT).show()
+            }
+        }
+    )
 
     Scaffold(topBar =
     {
@@ -63,20 +100,24 @@ fun EditProfileScreen(viewModel: AppViewModel, navController: NavHostController)
     }) {
         if (user.value != null) {
             Column(
-                modifier = Modifier.padding(it),
+                modifier = Modifier.padding(it).fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally
             ) {
                 Spacer(modifier = Modifier.height(40.dp))
                 if (user.value!!.profilePhoto.isNotEmpty()) {
                     AsyncImage(
                         model = user.value!!.profilePhoto, contentDescription = null,
-                        modifier = Modifier.size(150.dp)
+                        modifier = Modifier.size(150.dp).clickable {
+                            cameraPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+                        }
                     )
                 } else {
                     Image(
                         painter = painterResource(id = R.drawable.profile),
                         contentDescription = null,
-                        modifier = Modifier.size(150.dp)
+                        modifier = Modifier.size(150.dp).clickable {
+                            cameraPermissionLauncher.launch(android.Manifest.permission.CAMERA)
+                        }
                     )
                 }
                 Spacer(modifier = Modifier.height(20.dp))
